c8528f887160a579897225bb095533f964745ca5
lx <lx@lxtekiMacBook-Air.local>
2017-05-09 10:43:10 +0800

    Initial Commit

---
+++ myBGI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  myBGI
+//
+//  Created by lx on 2017/5/9.
+//  Copyright © 2017年 feng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ myBGI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  myBGI
+//
+//  Created by lx on 2017/5/9.
+//  Copyright © 2017年 feng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
